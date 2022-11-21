5977d81589f04eb9f45124d7646476b886b3cd34
dias <diaslaksmana@gmail.com>
2022-11-21 11:05:04 +0700

    updated header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+//const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Nunito': ['"Nunito"', 'sans-serif'],
+        'Work': ['"Work Sans"', 'sans-serif']
+      },
+      colors: {
+        'dark': '#0c111b',
+        'primary': '#00AFAA',
+        'secondary': '#7BC794',
+        'yellow': '#F4CD0C',
+        'grey': '#E8EDEF',
+        'white': '#FFF',
+        'red':'#dc2626'
+      },
+    },
   },
   plugins: [],
 }
